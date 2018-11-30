6e374576e94ab0beb70c7ef7faead79d3257522d
busyoucow <busyoucow@gmail.com>
2018-11-30 21:12:02 +0900

    BLEtest

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = 'deabb529-5f24-4f11-87ce-247c08ac7810'; // LED, Button
+const USER_SERVICE_UUID         = 'deabb529-5f24-4f11-87ce-247c08ac7810'; // LED, Buttontest
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
